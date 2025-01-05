1994457d131b7472d4506d90ad7f34b8449e848a
rojiwon123 <dev.rojiwon@gmail.com>
2025-01-05 17:53:12 +0900

    chore: api 문서 설명 업데이트

--- nestia.config.ts
+++ nestia.config.ts
@@ -9,6 +9,11 @@ const NESTIA_CONFIG: nestia.INestiaConfig = {
     primitive: true,
     json: false,
     swagger: {
+        info: {
+            title: "@ddi-ring/api",
+            description:
+                "띠링 API 서버 문서입니다. `version=0.0.0` 형태로 http 쿼리를 사용해 특정 버전의 문서를 확인할 수 있습니다.\n\n 동일한 스펙의 API Helper로 [`@ddi-ring/api`](https://github.com/ddi-ring/backend/pkgs/npm/api) 라이브러리도 배포되어 있습니다.",
+        },
         openapi: "3.0",
         decompose: true,
         output: "packages/doc/openapi.json",
