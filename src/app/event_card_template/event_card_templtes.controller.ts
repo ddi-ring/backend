004c8d9a12e7beae52526011a510a21b65eff355
rojiwon123 <dev.rojiwon@gmail.com>
2025-01-08 16:59:37 +0900

    feat: 이벤트 카드 템플릿 조회 api endpoint 추가

--- src/app/event_card_template/event_card_templtes.controller.ts
+++ src/app/event_card_template/event_card_templtes.controller.ts
@@ -2,8 +2,9 @@ import core from "@nestia/core";
 import * as nest from "@nestjs/common";
 
 import { notImpl } from "@/util/not_impl";
+import { Regex } from "@/util/type";
 
-import { EventCardTemplatePaginatedDTO, EventCardTemplateSearchDTO } from "./event_card_template.dto";
+import { EventCardTemplateDTO, EventCardTemplatePaginatedDTO, EventCardTemplateSearchDTO } from "./event_card_template.dto";
 
 @nest.Controller("event-card-templates")
 export class EventCardTemplatesController {
@@ -14,7 +15,7 @@ export class EventCardTemplatesController {
      *
      * cursor기반 pagination이 적용되어 있습니다.
      *
-     * @summary 이벤트 카드 템플릿 목록 보기
+     * @summary 이벤트 카드 템플릿 목록 조회
      * @tag event-card
      * @param query 검색 쿼리
      * @return 템플릿 목록
@@ -22,6 +23,22 @@ export class EventCardTemplatesController {
     @core.TypedRoute.Get()
     async paginate(@core.TypedQuery() query: EventCardTemplateSearchDTO): Promise<EventCardTemplatePaginatedDTO> {
         query;
+        return notImpl();
+    }
+
+    /**
+     * 이벤트 카드 템플릿 보기
+     *
+     * 이벤트 카드를 생성하기 위한 템플릿 정보를 조회합니다.
+     *
+     * @summary 이벤트 카드 템플릿 목록 조회
+     * @tag event-card
+     * @param template_id 템플릿 id
+     * @return 템플릿 정보
+     */
+    @core.TypedRoute.Get(":template_id")
+    async get(@core.TypedParam("template_id") template_id: Regex.UUID): Promise<EventCardTemplateDTO> {
+        template_id;
         return notImpl();
     }
 }
