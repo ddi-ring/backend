e521b00a0f7db3f0a5295eff8ccb3f26d2a5ff45
rojiwon123 <dev.rojiwon@gmail.com>
2025-01-15 17:36:39 +0900

    migration layer 재구성

--- prisma/migrations/20250115083625_init/migration.sql
+++ prisma/migrations/20250115083625_init/migration.sql
@@ -1,18 +1,10 @@
--- CreateTable
-CREATE TABLE "event_card_template" (
-    "id" UUID NOT NULL,
-    "thumbnail_image_id" UUID NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "created_at" TIMESTAMPTZ(6) NOT NULL,
-    "deleted_at" TIMESTAMPTZ(6),
-
-    CONSTRAINT "event_card_template_pkey" PRIMARY KEY ("id")
-);
+-- CreateEnum
+CREATE TYPE "EventCardFileType" AS ENUM ('thumbnail_image', 'gallery_image');
 
 -- CreateTable
 CREATE TABLE "event_card" (
     "id" UUID NOT NULL,
-    "template_id" UUID NOT NULL,
+    "template_key" TEXT NOT NULL,
     "thumbnail_image_id" UUID NOT NULL,
     "password" VARCHAR(50) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
@@ -25,6 +17,16 @@ CREATE TABLE "event_card" (
     "deleted_at" TIMESTAMPTZ(6),
 
     CONSTRAINT "event_card_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "event_card_file" (
+    "id" UUID NOT NULL,
+    "type" "EventCardFileType" NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL,
+    "deleted_at" TIMESTAMPTZ(6),
+
+    CONSTRAINT "event_card_file_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -54,13 +56,10 @@ CREATE TABLE "attachment_file" (
 );
 
 -- AddForeignKey
-ALTER TABLE "event_card_template" ADD CONSTRAINT "event_card_template_thumbnail_image_id_fkey" FOREIGN KEY ("thumbnail_image_id") REFERENCES "attachment_file"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event_card" ADD CONSTRAINT "event_card_thumbnail_image_id_fkey" FOREIGN KEY ("thumbnail_image_id") REFERENCES "event_card_file"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "event_card" ADD CONSTRAINT "event_card_template_id_fkey" FOREIGN KEY ("template_id") REFERENCES "event_card_template"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "event_card" ADD CONSTRAINT "event_card_thumbnail_image_id_fkey" FOREIGN KEY ("thumbnail_image_id") REFERENCES "attachment_file"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event_card_file" ADD CONSTRAINT "event_card_file_id_fkey" FOREIGN KEY ("id") REFERENCES "attachment_file"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "event_card_message" ADD CONSTRAINT "event_card_message_event_card_id_fkey" FOREIGN KEY ("event_card_id") REFERENCES "event_card"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
